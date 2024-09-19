acacaf419e782df8f02c72d6b2ef4db2dd717c4a
Idris Akintobi Adejare <akintobiidris@gmail.com>
2024-09-19 21:40:44 +0100

    separated custom errors and events

--- contracts/TokenSwap.sol
+++ contracts/TokenSwap.sol
@@ -2,8 +2,9 @@
 pragma solidity ^0.8.26;
 
 import "@openzeppelin/contracts/interfaces/IERC20.sol";
+import "./utils/CustomErrorAndEvents.sol";
 
-contract TokenSwap {
+contract TokenSwap is CustomErrorAndEvents {
     address private owner;
     struct Order {
         uint256 id;
@@ -23,46 +24,6 @@ contract TokenSwap {
     constructor() {
         owner = msg.sender;
     }
-
-    // Custom Errors
-    error OfferAmountMustBeGreaterThanZero();
-    error WantAmountMustBeGreaterThanZero();
-    error InsufficientAllowance(
-        address token,
-        address spender,
-        uint256 required
-    );
-    error InsufficientBalance(address token, address owner, uint256 required);
-    error OrderDoesNotExist(uint256 orderId);
-    error OrderNotApproved(uint256 orderId);
-    error OrderAlreadyFulfilled(uint256 orderId);
-    error OrderCanceledError(uint256 orderId);
-    error OnlyCreatorCanCancel(address caller);
-    error OnlyOwnerCanPerformThisAction();
-    error TransferFailed(address token);
-    error AddressZeroDetected();
-    error CanNotSwapSameToken();
-
-    // Events
-    event OrderCreated(
-        uint256 indexed id,
-        address indexed creator,
-        address offerToken,
-        uint256 offerAmount,
-        address wantToken,
-        uint256 wantAmount
-    );
-    event OrderFulfilled(
-        uint256 indexed id,
-        address indexed fulfiller,
-        address offerToken,
-        uint256 offerAmount,
-        address wantToken,
-        uint256 wantAmount
-    );
-    event OrderCanceled(uint256 indexed id, address indexed creator);
-    event OrderApproved(uint256 indexed id, address indexed owner);
-    event OrderDeclined(uint256 indexed id, address indexed owner);
 
     // Modifiers
     modifier onlyOwner() {
---
+++ contracts/utils/CustomErrorAndEvents.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import "@openzeppelin/contracts/interfaces/IERC20.sol";
+
+abstract contract CustomErrorAndEvents {
+    // Custom Errors
+    error OfferAmountMustBeGreaterThanZero();
+    error WantAmountMustBeGreaterThanZero();
+    error InsufficientAllowance(
+        address token,
+        address spender,
+        uint256 required
+    );
+    error InsufficientBalance(address token, address owner, uint256 required);
+    error OrderDoesNotExist(uint256 orderId);
+    error OrderNotApproved(uint256 orderId);
+    error OrderAlreadyFulfilled(uint256 orderId);
+    error OrderCanceledError(uint256 orderId);
+    error OnlyCreatorCanCancel(address caller);
+    error OnlyOwnerCanPerformThisAction();
+    error TransferFailed(address token);
+    error AddressZeroDetected();
+    error CanNotSwapSameToken();
+
+    // Events
+    event OrderCreated(
+        uint256 indexed id,
+        address indexed creator,
+        address offerToken,
+        uint256 offerAmount,
+        address wantToken,
+        uint256 wantAmount
+    );
+    event OrderFulfilled(
+        uint256 indexed id,
+        address indexed fulfiller,
+        address offerToken,
+        uint256 offerAmount,
+        address wantToken,
+        uint256 wantAmount
+    );
+    event OrderCanceled(uint256 indexed id, address indexed creator);
+    event OrderApproved(uint256 indexed id, address indexed owner);
+    event OrderDeclined(uint256 indexed id, address indexed owner);
+}
